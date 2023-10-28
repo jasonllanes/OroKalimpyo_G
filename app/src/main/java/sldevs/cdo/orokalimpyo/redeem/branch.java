767c2d3da1676d31d7cf6c09f7ba099c17a59dfb
Jason Llanes <llanezjaizon@gmail.com>
2023-10-28 11:46:48 -0700

    V.1.0.2

---
+++ app/src/main/java/sldevs/cdo/orokalimpyo/redeem/branch.java
@@ -0,0 +1,26 @@
+package sldevs.cdo.orokalimpyo.redeem;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import sldevs.cdo.orokalimpyo.R;
+
+public class branch extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_branch, container, false);
+
+
+
+        return v;
+    }
+}
